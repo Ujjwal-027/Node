4bad6d2e6171e433eb7976253b5f2352f94355f7
Ujjwal-027 <bhattaraiujwal09@gmail.com>
2025-07-11 21:57:50 +0545

    Models

--- controllers/testcontroller.js
+++ controllers/testcontroller.js
@@ -1,3 +1,27 @@
 exports.testfunction = (req, res) => {
     return res.send("Hello from testing function");
 }
+
+exports.testfunction = (req, res) => {
+    return res.send("Hello from testing function");
+}
+
+exports.testfunction = (req, res) => {
+    return res.send("Hello from testing function");
+}
+
+exports.testfunction = (req, res) => {
+    return res.send("Hello from testing function");
+}
+
+exports.testfunction = (req, res) => {
+    return res.send("Hello from testing function");
+}
+
+exports.testfunction = (req, res) => {
+    return res.send("Hello from testing function");
+}
+
+exports.testfunction = (req, res) => {
+    return res.send("Hello from testing function");
+}
--- database/connection.js
+++ database/connection.js
@@ -3,3 +3,27 @@ const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE)
 .then(()=> console.log('Database connected successfully'))
 .catch(error=> console.log(error))
+
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DATABASE)
+.then(()=> console.log('Database connected successfully'))
+.catch(error=> console.log(error))
+
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DATABASE)
+.then(()=> console.log('Database connected successfully'))
+.catch(error=> console.log(error))
+
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DATABASE)
+.then(()=> console.log('Database connected successfully'))
+.catch(error=> console.log(error))
+
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DATABASE)
+.then(()=> console.log('Database connected successfully'))
+.catch(error=> console.log(error))
